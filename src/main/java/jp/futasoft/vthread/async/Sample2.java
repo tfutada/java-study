97ea0148529c3d7364c594bfb18e7ca86c755e00
tfutada <tafutada@gmail.com>
2023-11-20 15:41:21 +0900

    init

--- src/main/java/jp/futasoft/vthread/async/Sample2.java
+++ src/main/java/jp/futasoft/vthread/async/Sample2.java
@@ -2,10 +2,11 @@ package jp.futasoft.vthread.async;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.*;
 
 public class Sample2 {
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
     // heavy task to be offloaded to another thread
     public static String compute(int n) {
         try {
@@ -21,10 +22,20 @@ public class Sample2 {
         return "task #" + n;
     }
 
+    // Simulates a non-blocking sleep and then returns a string
+    public static CompletableFuture<String> compute2(int n) {
+        CompletableFuture<String> future = new CompletableFuture<>();
+        scheduler.schedule(() -> future.complete("task #" + n), 5, TimeUnit.SECONDS);
+        return future;
+    }
+
     public static CompletableFuture<String> create(int n) {
         return CompletableFuture.supplyAsync(() -> {
             return compute(n);
         });
+    }
+    public static CompletableFuture<String> create2(int n) {
+        return compute2(n);
     }
 
     //
@@ -36,7 +47,7 @@ public class Sample2 {
         // create 10 threads with a loop
         for (int i = 0; i < 1000; i++) {
             // fire and forget
-            var job = create(i)
+            var job = create2(i)
                     .thenAccept(System.out::println)
                     .thenRun(() -> System.out.println("Done!"));
             jobs.add(job);
