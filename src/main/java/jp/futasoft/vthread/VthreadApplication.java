c9e2ef8518ec2b0bc776cba0ce2ea209048536d5
tfutada <tafutada@gmail.com>
2023-11-20 10:32:25 +0900

    init

---
+++ src/main/java/jp/futasoft/vthread/VthreadApplication.java
@@ -0,0 +1,13 @@
+package jp.futasoft.vthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VthreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VthreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/futasoft/vthread/VthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.futasoft.vthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VthreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
