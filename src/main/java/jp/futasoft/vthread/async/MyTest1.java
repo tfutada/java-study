9c7057f71968f1c95f42f26f7c30001c2deade02
tfutada <tafutada@gmail.com>
2023-11-20 10:35:44 +0900

    init

--- src/main/java/jp/futasoft/vthread/async/MyTest1.java
+++ src/main/java/jp/futasoft/vthread/async/MyTest1.java
@@ -1,14 +1,14 @@
-package jp.futasoft.vthread;
+package jp.futasoft.vthread.async;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VthreadApplication {
+public class MyTest1 {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(VthreadApplication.class, args);
+		SpringApplication.run(MyTest1.class, args);
 		// print hello world
 		System.out.println("Hello World!");
 	}
