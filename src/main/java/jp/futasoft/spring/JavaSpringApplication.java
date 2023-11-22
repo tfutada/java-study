48ccd458150963fea0325a4843046bc87c9d3aa5
tfutada <tafutada@gmail.com>
2023-11-22 18:44:02 +0900

    init

---
+++ src/main/java/jp/futasoft/spring/JavaSpringApplication.java
@@ -0,0 +1,62 @@
+package jp.futasoft.spring;
+
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executors;
+
+@SpringBootApplication
+public class JavaSpringApplication {
+    private static final Logger log = LoggerFactory.getLogger(JavaSpringApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringApplication.class, args);
+    }
+
+    @Bean
+    @SneakyThrows
+    CommandLineRunner runner() {
+        return args -> {
+            System.out.println("!!! started...");
+
+            var n = Runtime.getRuntime().availableProcessors();
+            log.info("!!! availableProcessors: {}", n);
+
+            try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+                var process = executor.submit(() -> {
+                    log.info("!!! Hello, World");
+                });
+
+                process.get();
+
+            } catch (Exception e) {
+                log.error("Error", e);
+            }
+        };
+    }
+}
+
+//@Controller
+//class HelloController {
+//    @RequestMapping("/")
+//    @ResponseBody
+//    String home() {
+//        return "Hello, Worlduuu";
+//    }
+//}
+//
+//@RestController
+//class HelloJsonController {
+//    @RequestMapping("/json")
+//    public Greeting home() {
+//        return new Greeting("Hello, Worlduuu");
+//    }
+//
+//    public record Greeting(String message) {
+//    }
+//}
---
+++ src/main/java/jp/futasoft/vthread/hello/Hello.java
@@ -0,0 +1,9 @@
+package jp.futasoft.vthread.hello;
+
+
+
+public class Hello {
+    public void main() {
+        System.out.println("Hello Java");
+    }
+}
