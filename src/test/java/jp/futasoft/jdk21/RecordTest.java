f027eaf6c955decd2c7eced1f64b2a1765372d71
tfutada <tafutada@gmail.com>
2023-11-25 11:37:44 +0900

    JDK 21

---
+++ src/test/java/jp/futasoft/jdk21/RecordTest.java
@@ -0,0 +1,19 @@
+package jp.futasoft.jdk21;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RecordTest {
+
+    record JdkReleasedEvent(String name) {
+    }
+
+    @Test
+    void records() throws Exception {
+        var event = new JdkReleasedEvent("Java21");
+        Assertions.assertEquals(event.name(), "Java21");
+        System.out.println(event);
+
+    }
+}
